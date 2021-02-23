add2ef55e040383fe66f7d3f63e7334d6f6cdf95
murata0531 <coless115@gmail.com>
2021-02-23 16:23:04 +0900

    ok

--- djangopj/settings.py
+++ djangopj/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'od!j8x%-s3nk)d-sj@cgsnk35y-s0jc--kftjr=$o_^$9-i4!h'
+
+from .setting_local import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
